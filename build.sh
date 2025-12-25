c7405b6570f18cd9e33ef7c31c89d9035efdc6b9
xiaofanqie <zstomato2014@gmail.com>
2025-12-25 17:12:25 +0800

    Update build.sh
    Signed-off-by: xiaofanqie <zstomato2014@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2025 ZqinKing
+# Copyright (C) 2025 zstomato
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
